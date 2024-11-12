539b9c447308d15a795998851695d73a07ba6112
Talha <mdtalha2008@gmail.com>
2024-11-13 01:27:37 +0600

    text upate

--- src/pages/Home/Medium/Medium.jsx
+++ src/pages/Home/Medium/Medium.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 const Medium = () => {
     return (
-        <div className="flex flex-col items-center justify-center  bg-black text-white">
-            <h1 className="text-xl font-light">ChatGPT</h1>
-            <h2 className="text-4xl font-bold mt-4 mb-2 text-center">
+        <div className="flex flex-col items-center justify-center h-80  bg-black text-white">
+            <h1 className="text-xl font-light" style={{ fontFamily: "Atma, sans-serif", fontWeight: 400 }}> বাংলাGPT</h1>
+            <h2 className="text-4xl font-extrabold mt-4 mb-2 text-center" style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}>
                 Get answers. Find inspiration. <br /> Be more productive.
             </h2>
             <p className="text-lg font-light mt-2 mb-8 text-center max-w-md">
-                Free to use. Easy to try. Just ask and ChatGPT can help with writing, learning, brainstorming, and more.
+               বাংলা GPT এমন একটি প্ল্যাটফর্ম, যেখানে আপনি বাংলা প্রম্পটের মাধ্যমে সহজেই বাংলায় ChatGPT-এর ব্যবহার আরও কার্যকরভাবে করতে পারবেন।
             </p>
             <div className="flex gap-4">
                 <button className="px-6 py-3 bg-white text-black rounded-full font-medium hover:bg-gray-300">
