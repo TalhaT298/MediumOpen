e1b0581f7e6defc1b8c4cf188313fb4bd7292f68
Talha <mdtalha2008@gmail.com>
2024-11-13 00:45:17 +0600

    nvabr update

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,30 +1,40 @@
-
-
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 import { AiFillEnvironment } from "react-icons/ai";
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100">
-  <div className="flex-1">
-  <a className="btn btn-ghost text-xl" style={{ fontFamily: 'Atma, sans-serif', fontWeight: 400 }}>ChatGPT বাংলা</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li>
-        <details>
-          <summary><AiFillEnvironment /></summary>
+      <div className="navbar">
+        <div className="flex-1">
+          <a
+            className="text-xl"
+            style={{ fontFamily: "Atma, sans-serif", fontWeight: 400 }}
+          >
+            ChatGPT বাংলা
+          </a>
+        </div>
+        <div className="flex-none ">
+          <ul className="menu-horizontal gap-x-4">
+            <li>
+              <a>Link</a>
+            </li>
+            <li>
+              <a>
+                <AiFillEnvironment className="w-6 h-6" />
+              </a>
+            </li>
+            <li>
+              {/* <details>
+          <summary></summary>
           <ul className="bg-base-100 rounded-t-none p-2">
             <li><a>Link 1</a></li>
             <li><a>Link 2</a></li>
           </ul>
-        </details>
-      </li>
-    </ul>
-  </div>
-</div>
+        </details> */}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
