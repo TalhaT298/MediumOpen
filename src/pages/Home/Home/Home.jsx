ed63cfeea8581e695825f24f3800ec8529d583ea
Talha <mdtalha2008@gmail.com>
2024-11-13 21:39:50 +0600

    home update

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import Medium from '../Medium/Medium';
 import Technology from '../Technology/Technology';
+import BanglaGPT from '../BanglaGPT/BanglaGPT';
 
 const Home = () => {
     return (
         <div>
             <Medium></Medium>
             <Technology></Technology>
+            <BanglaGPT></BanglaGPT>
         </div>
     );
 };
