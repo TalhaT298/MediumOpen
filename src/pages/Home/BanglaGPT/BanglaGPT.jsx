28eaac25b8326e7ccc2f25f99e85778d4c96b1b8
Talha <mdtalha2008@gmail.com>
2024-11-13 22:02:09 +0600

    banglagpt title add

--- src/pages/Home/BanglaGPT/BanglaGPT.jsx
+++ src/pages/Home/BanglaGPT/BanglaGPT.jsx
@@ -6,8 +6,8 @@ const BanglaGPT = () => {
         <>
         <div className="flex items-center justify-center  bg-black text-center text-white">
             <div>
-                <h1 className="text-3xl md:text-5xl font-semibold mb-4">
-                    Writes, brainstorms, edits,<br /> and explores ideas with BanglaGPT
+                <h1 className="text-3xl md:text-5xl font-semibold mb-4" style={{ fontFamily: "Atma, sans-serif", fontWeight: 400 }}>
+                    Writes, brainstorms, edits,<br /> and explores ideas with বাংলাGPT
                 </h1>
                 <p className="text-gray-400 underline text-lg mt-4 cursor-pointer">
                     Learn more about writing with ChatGPT &gt;
