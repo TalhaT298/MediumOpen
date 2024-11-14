ffaf15c4e6a50e9b50c8cb67879df186949bd403
Talha <mdtalha2008@gmail.com>
2024-11-15 00:05:32 +0600

    try middle add

--- src/pages/Home/Try/Try.jsx
+++ src/pages/Home/Try/Try.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 const Try = () => {
     return (
         <div>
-            <div className="flex items-center justify-center min-h-screen bg-gray-900">
-    <div className="p-10 bg-gray-800 text-center rounded-lg">
-        <p className="text-white text-2xl font-semibold mb-6">Join hundreds of millions of users and try ChatGPT today.</p>
+            <div className="flex items-center justify-center py-10">
+    <div className="p-10 border border-white text-center rounded-lg">
+        <p className="text-white text-2xl font-semibold mb-6" style={{ fontFamily: "Atma, sans-serif", fontWeight: 500 }}>Join hundreds of millions of users and try বাংলাGPT today.</p>
         <button className="px-6 py-2 bg-white text-gray-800 font-medium rounded-full hover:bg-gray-200 transition">
             Try ChatGPT <span className="ml-1">↗️</span>
         </button>
