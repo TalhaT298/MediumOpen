525bf4a4ab1733b7f618dcb3dbd25adc459fac8d
Talha <mdtalha2008@gmail.com>
2024-11-14 21:22:46 +0600

    prompt add

--- src/pages/Home/Prompt/Prompt.jsx
+++ src/pages/Home/Prompt/Prompt.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 
 const Prompt = () => {
   return (
@@ -9,11 +10,28 @@ const Prompt = () => {
         {/* Left Section */}
         <div className="w-3/5 flex flex-col border border-white">
           <div className="h-3/5  border-b border-white">
-            {/* Upper Left (60% of left section) */}
+            <div
+              className="flex flex-col text-xl text-center justify-center items-center"
+              style={{ fontFamily: "Atma, sans-serif", fontWeight: 400 }}
+            >
+              <p>
+                আপনি কি আমাকে{" "}
+                <span className="font-extrabold">[বিষয় বা নিবন্ধ]</span> এর মূল
+                বিষয়বস্তু সংক্ষেপে বলতে পারেন?
+              </p>
+              <p>
+                উদাহরণ: "আপনি কি আমাকে জলবায়ু পরিবর্তন সম্পর্কিত নিবন্ধটির মূল
+                বিষয়গুলো সংক্ষেপে বলতে পারেন?"
+              </p>
+              <p>
+                ব্যাখ্যা: এটি দীর্ঘ পাঠকে সংক্ষিপ্ত আকারে জানার সুযোগ দেয়, মূল
+                ধারণাগুলি সহজে বোঝা যায়।
+              </p>
+            </div>
           </div>
           <div className="h-2/5 ">
-            {/* Lower Left (40% of left section) */}
-          </div>
+          
+          {/* Lower Left (40% of left section) */}</div>
         </div>
 
         {/* Right Section */}
@@ -37,15 +55,11 @@ const Prompt = () => {
           <div className="flex-1   border-b border-white">
             {/* 2nd Left Part */}
           </div>
-          <div className="flex-1 ">
-            {/* 3rd Left Part */}
-          </div>
+          <div className="flex-1 ">{/* 3rd Left Part */}</div>
         </div>
 
         {/* Right Part: Full Height */}
-        <div className="w-1/2 ">
-          {/* Right Part */}
-        </div>
+        <div className="w-1/2 ">{/* Right Part */}</div>
       </div>
 
       {/* New Bottom Section ---------*/}
@@ -55,21 +69,15 @@ const Prompt = () => {
           <div className="flex-1  border-b border-white">
             {/* Upper Right Part */}
           </div>
-          <div className="flex-1 ">
-            {/* Lower Right Part */}
-          </div>
+          <div className="flex-1 ">{/* Lower Right Part */}</div>
         </div>
         {/* Left Part: One Full Section */}
         <div className="w-3/5 flex flex-col border border-white">
           <div className="h-3/5  border-b border-white">
             {/* Upper Left (60% of left section) */}
           </div>
-          <div className="h-2/5 ">
-            {/* Lower Left (40% of left section) */}
-          </div>
+          <div className="h-2/5 ">{/* Lower Left (40% of left section) */}</div>
         </div>
-
-        
       </div>
     </div>
   );
