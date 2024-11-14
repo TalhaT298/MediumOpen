e2ad65c35c58eb5a5230ec967545265722417793
Talha <mdtalha2008@gmail.com>
2024-11-14 23:47:31 +0600

    try add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,6 +4,7 @@ import Medium from '../Medium/Medium';
 import Technology from '../Technology/Technology';
 import BanglaGPT from '../BanglaGPT/BanglaGPT';
 import Prompt from '../Prompt/Prompt';
+import Try from '../Try/Try';
 
 const Home = () => {
     return (
@@ -12,6 +13,7 @@ const Home = () => {
             <Technology></Technology>
             <BanglaGPT></BanglaGPT>
             <Prompt></Prompt>
+            <Try></Try>
         </div>
     );
 };
