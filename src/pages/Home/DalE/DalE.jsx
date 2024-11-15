5e18ba48d8a97c60f797288f0946ba491e0e647c
Talha <mdtalha2008@gmail.com>
2024-11-15 22:25:50 +0600

    dale add

---
+++ src/pages/Home/DalE/DalE.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const DalE = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DalE;
--- src/pages/Home/Try/Try.jsx
+++ src/pages/Home/Try/Try.jsx
@@ -1,21 +1,25 @@
 /* eslint-disable no-unused-vars */
-import { RiseOutlined } from '@ant-design/icons';
-import React from 'react';
+import { RiseOutlined } from "@ant-design/icons";
+import React from "react";
 
 const Try = () => {
-    return (
-        <div className='pt-10'>
-            <div className="flex items-center justify-center bg-zinc-900 rounded-md py-10">
-    <div className="p-10 text-center rounded-lg">
-        <p className="text-white text-4xl font-semibold mb-6" style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}>Join hundreds of millions of <br /> users and try বাংলাGPT today.</p>
-        <button className="px-6 py-2 bg-white text-black font-medium rounded-full hover:bg-gray-200 transition">
-            Try বাংলাGPT  <RiseOutlined />
-        </button>
-    </div>
-</div>
-
+  return (
+    <div className="pt-10">
+      <div className="flex items-center justify-center bg-zinc-900 rounded-md py-10">
+        <div className="p-10 text-center rounded-lg">
+          <p
+            className="text-white text-4xl font-semibold mb-6"
+            style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}
+          >
+            Join hundreds of millions of <br /> users and try বাংলাGPT today.
+          </p>
+          <button className="px-6 py-2 bg-white text-black font-medium rounded-full hover:bg-gray-200 transition">
+            Try বাংলাGPT <RiseOutlined />
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Try;
