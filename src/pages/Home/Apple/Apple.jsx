479b8dc685cc038275d4d166cf44fcd1f35b8069
Talha <mdtalha2008@gmail.com>
2024-11-15 22:41:32 +0600

    apple add

--- src/pages/Home/Apple/Apple.jsx
+++ src/pages/Home/Apple/Apple.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import chat7 from "../../../assets/banner7.png";
-const DalE = () => {
-    return (
-        <div className="py-10">
+import React from "react";
+import chat8 from "../../../assets/banner8.png";
+const Apple = () => {
+  return (
+    <div className="py-10">
       <div className="flex items-center justify-center ">
         <div className="w-2/5 p-8 text-white bg-zinc-900 h-[550px]">
           <div className="flex items-center justify-center ">
             <div className="pt-40">
               <div className="flex flex-col items-start">
-                <h1 className="text-3xl font-bold mb-4">Create images</h1>
+                <h1 className="text-3xl font-bold mb-4">Apple & ChatGPT</h1>
                 <p className="text-xl items-start">
-                Ask ChatGPT to create images using a simple sentence or detailed paragraph.
+                  At WWDC in June 2024, we announced a partnership with Apple to
+                  integrate ChatGPT into experiences within iOS, iPadOS, and
+                  macOS.
                 </p>
+                <a href=""><p>Learn More</p></a>
               </div>
             </div>
           </div>
@@ -21,13 +24,13 @@ const DalE = () => {
         <div className="w-2/5 flex items-center justify-center bg-blue-100">
           <div className="b   h-[545px]  flex items-center justify-center">
             <div className="w-[520px] h-[550px] ">
-              <img src={chat7} alt="" />
+              <img src={chat8} alt="" />
             </div>
           </div>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
-export default DalE;
+export default Apple;
