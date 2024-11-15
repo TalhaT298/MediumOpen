983dc222c2dbc4321c3f5f8f441c3b9be54d0a42
Talha <mdtalha2008@gmail.com>
2024-11-15 22:21:25 +0600

    try add baglagpt

--- src/pages/Home/Try/Try.jsx
+++ src/pages/Home/Try/Try.jsx
@@ -9,7 +9,7 @@ const Try = () => {
     <div className="p-10 text-center rounded-lg">
         <p className="text-white text-4xl font-semibold mb-6" style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}>Join hundreds of millions of <br /> users and try বাংলাGPT today.</p>
         <button className="px-6 py-2 bg-white text-black font-medium rounded-full hover:bg-gray-200 transition">
-            Try ChatGPT <RiseOutlined />
+            Try বাংলাGPT  <RiseOutlined />
         </button>
     </div>
 </div>
