afa13b460fc67cd6acf49b157a04e93f5a93726a
Talha <mdtalha2008@gmail.com>
2024-11-15 17:54:22 +0600

    try add

--- src/pages/Home/Try/Try.jsx
+++ src/pages/Home/Try/Try.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { RiseOutlined } from '@ant-design/icons';
 import React from 'react';
 
 const Try = () => {
@@ -8,7 +9,7 @@ const Try = () => {
     <div className="p-10 text-center rounded-lg">
         <p className="text-white text-4xl font-semibold mb-6" style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}>Join hundreds of millions of <br /> users and try বাংলাGPT today.</p>
         <button className="px-6 py-2 bg-white text-black font-medium rounded-full hover:bg-gray-200 transition">
-            Try ChatGPT <span className="ml-1">↗️</span>
+            Try ChatGPT <RiseOutlined />
         </button>
     </div>
 </div>
