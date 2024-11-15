56730f7d5776f05bbb918cd5640d5ceed62c0c4d
Talha <mdtalha2008@gmail.com>
2024-11-15 21:58:36 +0600

    chart add

---
+++ src/pages/Home/Chart/Chart.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Chart = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Chart;
