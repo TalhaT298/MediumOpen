cf55c436199ff8aa9af817e0b5e1cae3ee58f780
Talha <mdtalha2008@gmail.com>
2024-11-15 22:05:36 +0600

    chart at

--- src/pages/Home/Chart/Chart.jsx
+++ src/pages/Home/Chart/Chart.jsx
@@ -1,10 +1,37 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-
+import chat5 from "../../../assets/banner5.png";
 const Chart = () => {
     return (
         <div>
-            
+            <div className="py-10">
+      
+      <div className="flex items-center justify-center ">
+        <div className="w-2/5 p-8 text-white bg-zinc-900 h-[550px]">
+          <div className="flex items-center justify-center ">
+            <div className="pt-40">
+              <div className="flex flex-col items-start">
+                <h1 className="text-2xl font-bold mb-4">
+                Analyze data and create charts
+                </h1>
+                <p className="text-xl items-start">
+                Upload a file and ask ChatGPT to help analyze <br /> data, summarize information or create a chart.
+                </p>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-2/5 flex items-center justify-center bg-blue-100">
+          <div className="bg-white  w- h-[550px]  flex items-center justify-center">
+            <div className="w-[520px] h-[550px] ">
+              <img src={chat5} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
         </div>
     );
 };
