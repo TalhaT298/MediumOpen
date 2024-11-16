f760774182e6c8c39b8b49a014d8a077eb1d4a36
Talha <mdtalha2008@gmail.com>
2024-11-16 12:55:10 +0600

    learn edit done

--- src/pages/Home/Edit/Edit.jsx
+++ src/pages/Home/Edit/Edit.jsx
@@ -1,33 +1,76 @@
+// /* eslint-disable no-unused-vars */
+// import React from 'react';
+// import banner2 from "../../../assets/banner2.png";
+// const Edit = () => {
+//     return (
+//         <>
+//       <div className="flex items-center justify-center  bg-black text-center text-white pt-14">
+//         <div>
+//           <h1
+//             className="text-3xl md:text-5xl font-semibold mb-4"
+//             style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}
+//           >
+//             Summarize meetings. Find new <br /> insights. Increase productivity.
+//           </h1>
+//           <a
+//             href="https://chatgpt.com/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             <p className="text-white underline text-lg mt-4 cursor-pointer">
+//               Learn more about writing with ChatGPT &gt;
+//             </p>
+//           </a>
+//         </div>
+//       </div>
+//       <div className="flex justify-center items-center h-screen">
+//         <img className="w-4/5" src={banner2} alt="" />
+//       </div>
+//     </>
+//     );
+// };
+
+// export default Edit;
+
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 import banner2 from "../../../assets/banner2.png";
+
 const Edit = () => {
-    return (
-        <>
-      <div className="flex items-center justify-center  bg-black text-center text-white pt-14">
+  return (
+    <>
+      {/* Hero Section */}
+      <div className="flex flex-col items-center justify-center bg-black text-center text-white pt-14 px-4">
         <div>
           <h1
-            className="text-3xl md:text-5xl font-semibold mb-4"
+            className="text-xl md:text-3xl lg:text-5xl font-semibold mb-4"
             style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}
           >
-            Summarize meetings. Find new <br /> insights. Increase productivity.
+            Summarize meetings. Find new <br className="hidden md:block" />
+            insights. Increase productivity.
           </h1>
           <a
             href="https://chatgpt.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <p className="text-white underline text-lg mt-4 cursor-pointer">
+            <p className="text-white underline text-sm md:text-lg mt-4 cursor-pointer">
               Learn more about writing with ChatGPT &gt;
             </p>
           </a>
         </div>
       </div>
-      <div className="flex justify-center items-center h-screen">
-        <img className="w-4/5" src={banner2} alt="" />
+
+      {/* Banner Section */}
+      <div className="flex justify-center items-center h-auto md:h-screen p-4">
+        <img
+          className="w-full md:w-4/5 object-contain"
+          src={banner2}
+          alt="Edit Banner"
+        />
       </div>
     </>
-    );
+  );
 };
 
 export default Edit;
--- src/pages/Home/Learn/Learn.jsx
+++ src/pages/Home/Learn/Learn.jsx
@@ -1,33 +1,76 @@
+// /* eslint-disable no-unused-vars */
+// import React from 'react';
+// import banner4 from "../../../assets/banner4.png";
+// const Learn = () => {
+//     return (
+//         <>
+//       <div className="flex items-center justify-center  bg-black text-center text-white pt-14">
+//         <div>
+//           <h1
+//             className="text-3xl md:text-5xl font-semibold mb-4"
+//             style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}
+//           >
+//             Learn something new. Dive into a <br /> hobby. Answer complex questions.
+//           </h1>
+//           <a
+//             href="https://chatgpt.com/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             <p className="text-white underline text-lg mt-4 cursor-pointer">
+//               Learn more about writing with ChatGPT &gt;
+//             </p>
+//           </a>
+//         </div>
+//       </div>
+//       <div className="flex justify-center items-center h-screen">
+//         <img className="w-4/5" src={banner4} alt="" />
+//       </div>
+//     </>
+//     );
+// };
+
+// export default Learn;
+
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 import banner4 from "../../../assets/banner4.png";
+
 const Learn = () => {
-    return (
-        <>
-      <div className="flex items-center justify-center  bg-black text-center text-white pt-14">
+  return (
+    <>
+      {/* Hero Section */}
+      <div className="flex flex-col items-center justify-center bg-black text-center text-white pt-14 px-4">
         <div>
           <h1
-            className="text-3xl md:text-5xl font-semibold mb-4"
+            className="text-xl md:text-3xl lg:text-5xl font-semibold mb-4"
             style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}
           >
-            Learn something new. Dive into a <br /> hobby. Answer complex questions.
+            Learn something new. Dive into a <br className="hidden md:block" />
+            hobby. Answer complex questions.
           </h1>
           <a
             href="https://chatgpt.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <p className="text-white underline text-lg mt-4 cursor-pointer">
+            <p className="text-white underline text-sm md:text-lg mt-4 cursor-pointer">
               Learn more about writing with ChatGPT &gt;
             </p>
           </a>
         </div>
       </div>
-      <div className="flex justify-center items-center h-screen">
-        <img className="w-4/5" src={banner4} alt="" />
+
+      {/* Banner Section */}
+      <div className="flex justify-center items-center h-auto md:h-screen p-4">
+        <img
+          className="w-full md:w-4/5 object-contain"
+          src={banner4}
+          alt="Learn Banner"
+        />
       </div>
     </>
-    );
+  );
 };
 
 export default Learn;
